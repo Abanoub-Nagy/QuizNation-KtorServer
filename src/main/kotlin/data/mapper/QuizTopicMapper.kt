82e4f64b62d7ac4b958edfd08f20ed7d2417696e
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-03-24 07:59:16 +0200

    Quiz Topic Functions added

---
+++ src/main/kotlin/data/mapper/QuizTopicMapper.kt
@@ -0,0 +1,17 @@
+package com.example.data.mapper
+
+import com.example.data.database.entity.QuizTopicEntity
+import com.example.domin.model.QuizTopic
+
+fun QuizTopicEntity.toQuizTopic() = QuizTopic(
+    id = _id,
+    name = name,
+    code = code,
+    imageUrl = imageUrl
+)
+
+fun QuizTopic.toQuizTopicEntity() = QuizTopicEntity(
+    name = name,
+    code = code,
+    imageUrl = imageUrl
+)
--- src/main/kotlin/data/util/Constant.kt
+++ src/main/kotlin/data/util/Constant.kt
@@ -3,4 +3,5 @@ package com.example.data.util
 object Constant {
     const val MONGO_DATABASE_NAME = "QuizNotion_db"
     const val QUESTIONS_COLLECTION_NAME = "quiz_questions"
+    const val TOPIC_COLLECTION_NAME = "quiz_topics"
 }

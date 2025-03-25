95f6c14732552d5806e971e64b01e6da490f2c74
Abanoub-Nagy <abanoubnagy24@gmail.com>
2025-03-25 04:42:38 +0200

    Issue Report Functions added

---
+++ src/main/kotlin/data/mapper/IssueReportMapper.kt
@@ -0,0 +1,21 @@
+package com.example.data.mapper
+
+import com.example.data.database.entity.IssueReportEntity
+import com.example.domin.model.IssueReport
+
+fun IssueReportEntity.toIssueReport() = IssueReport(
+    id = _id,
+    quizQuestionId = quizQuestionId,
+    issueType = issueType,
+    additionalComment = additionalComment,
+    userEmail = userEmail,
+    timestamp = timestamp
+)
+
+fun IssueReport.IssueReportEntity() = IssueReportEntity(
+    quizQuestionId = quizQuestionId,
+    issueType = issueType,
+    additionalComment = additionalComment,
+    userEmail = userEmail,
+    timestamp = timestamp
+)
--- src/main/kotlin/data/util/Constant.kt
+++ src/main/kotlin/data/util/Constant.kt
@@ -4,4 +4,5 @@ object Constant {
     const val MONGO_DATABASE_NAME = "QuizNotion_db"
     const val QUESTIONS_COLLECTION_NAME = "quiz_questions"
     const val TOPIC_COLLECTION_NAME = "quiz_topics"
+    const val ISSUE_REPORTS_COLLECTION_NAME = "issue_reports"
 }
